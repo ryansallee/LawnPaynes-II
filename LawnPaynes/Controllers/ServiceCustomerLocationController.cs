6ecc2eda9b1c10fe96564ad551e9ec7288cb2138
Ryan Sallee <rcsall11@g.holycross.edu>
2018-07-14 18:12:09 -0400

    Setting Up Controller to Create Add Page for the ServiceCustomerLocation section.

--- LawnPaynes/Controllers/ServiceCustomerLocationController.cs
+++ LawnPaynes/Controllers/ServiceCustomerLocationController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using LawnPaynes.Models;
+using LawnPaynes.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace LawnPaynes.Controllers
 {
@@ -16,6 +19,41 @@ namespace LawnPaynes.Controllers
             Context.SaveChanges();
 
             return RedirectToAction("Detail", "Customer", new { id = customerId });
+        }
+
+        public ActionResult Add(int customerLocationId)
+        {
+            var customerLocation = Context.CustomerLocations
+                .Where(cl => cl.CustomerLocationId == customerLocationId)
+                .SingleOrDefault();
+
+            if (customerLocation == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new ServiceCustomerLocationAddViewModel()
+            {
+                CustomerLocation = customerLocation
+            };
+
+            viewModel.Initialize(Context);
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Add(ServiceCustomerLocationAddViewModel viewModel)
+        {
+            var serviceCustomerLocation = new ServiceCustomerLocation()
+            {
+                CustomerLocationId = viewModel.CustomerLocationId,
+                ServiceId = viewModel.ServiceId
+            };
+
+            Context.ServiceCustomerLocations.Add(serviceCustomerLocation);
+
+            return RedirectToAction("Detail", "Customer", new { id = viewModel.CustomerLocation.CustomerId });
+
         }
     }
 }
---
+++ LawnPaynes/ViewModels/ServiceCustomerLocationAddViewModel.cs
@@ -0,0 +1,33 @@
+﻿using LawnPaynes.Data;
+using LawnPaynes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace LawnPaynes.ViewModels
+{
+    public class ServiceCustomerLocationAddViewModel
+    {
+        public int CustomerLocationId
+        {
+            get { return CustomerLocation.CustomerLocationId; }
+            set { CustomerLocation.CustomerLocationId = value; }
+        }
+
+        public CustomerLocation CustomerLocation { get; set; } = new CustomerLocation();
+        public int ServiceId { get; set; }
+
+
+
+        public SelectList ServiceSelectListItems { get; set; }
+
+        public void Initialize(LawnPaynesContext context)
+        {
+            var serviceList = context.Services.ToList();
+            ServiceSelectListItems = new SelectList(serviceList, "ServiceId", "ServiceName");
+        }
+    }
+}
