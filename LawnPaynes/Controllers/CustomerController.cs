1483768630cb09a990f73f3a96ca008b37624e82
Ryan Sallee <rcsall11@g.holycross.edu>
2018-07-21 15:10:31 -0400

    Added serverside validation for Customer Add.

--- LawnPaynes/Controllers/CustomerController.cs
+++ LawnPaynes/Controllers/CustomerController.cs
@@ -1,10 +1,5 @@
-﻿using LawnPaynes.Data;
-using LawnPaynes.Models;
-using LawnPaynes.ViewModels;
-using System;
-using System.Collections.Generic;
+﻿using LawnPaynes.Models;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
 using System.Net;
@@ -13,7 +8,7 @@ namespace LawnPaynes.Controllers
 {
     public class CustomerController : BaseController
     {
-  
+
         public ActionResult Index()
         {
             var customers = Context.Customers.
@@ -32,89 +27,102 @@ namespace LawnPaynes.Controllers
         [HttpPost]
         public ActionResult Add(Customer customer)
         {
+            CustomerValidator(customer);
+
             if (ModelState.IsValid)
-            {                         
+            {
                 Context.Customers.Add(customer);
                 Context.SaveChanges();
 
-                TempData["Message"] =  customer.Name + " was successfully added!";
+                TempData["Message"] = customer.Name + " was successfully added!";
                 return RedirectToAction("Index");
             }
 
             return View(customer);
         }
 
-            public ActionResult Detail(int? id)
+        public ActionResult Detail(int? id)
 
+        {
+            if (id == null)
             {
-                if (id == null)
-                {
-                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-                }
-
-                var customer = Context.Customers
-                .Include(c => c.CustomerLocations)
-                .Include("CustomerLocations.ServiceCustomerLocations.Service")
-                .Where(c => c.CustomerId == id)
-                .SingleOrDefault();
-
-                
-                if (customer == null)
-                {
-                    return HttpNotFound();
-                }
-
-                customer.CustomerLocations = customer.CustomerLocations.ToList();
-
-                 return View(customer);
-             }
-
-            public ActionResult Edit(int? id)
-            {
-                if (id == null)
-                {
-                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-                }
-
-                var customer = Context.Customers
-                .Where(c => c.CustomerId == id)
-                .SingleOrDefault();
-
-                if (customer == null)
-                {
-                    return HttpNotFound();
-                }
-
-                
-                return View(customer);
-
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            [HttpPost]
-            public ActionResult Edit(Customer customer)
+            var customer = Context.Customers
+            .Include(c => c.CustomerLocations)
+            .Include("CustomerLocations.ServiceCustomerLocations.Service")
+            .Where(c => c.CustomerId == id)
+            .SingleOrDefault();
+
+
+            if (customer == null)
             {
-                if (ModelState.IsValid)
-                {
-                    Context.Entry(customer).State = EntityState.Modified;
-                    Context.SaveChanges();
-
-                TempData["Message"] = customer.Name + " was successfully updated!";
-                return RedirectToAction("Detail", new {id = customer.CustomerId});
-                }
-
-                return View(customer);
+                return HttpNotFound();
             }
+
+            customer.CustomerLocations = customer.CustomerLocations.ToList();
+
+            return View(customer);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var customer = Context.Customers
+            .Where(c => c.CustomerId == id)
+            .SingleOrDefault();
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+
+            return View(customer);
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Customer customer)
+        {
+            Context.Entry(customer).State = EntityState.Modified;
+            Context.SaveChanges();
+
+            TempData["Message"] = customer.Name + " was successfully updated!";
+            return RedirectToAction("Detail", new { id = customer.CustomerId });
             
+        }
 
-            [HttpPost]
-            public ActionResult Delete(int id)
-            {
-                var customer = Context.Customers.Find(id);
-                Context.Customers.Remove(customer);
-                Context.SaveChanges();
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var customer = Context.Customers.Find(id);
+            Context.Customers.Remove(customer);
+            Context.SaveChanges();
 
             TempData["Message"] = customer.Name + " was successfully deleted!";
             return RedirectToAction("Index");
+        }
+
+        private void CustomerValidator(Customer customer)
+        {
+            if (ModelState.IsValidField("Name") && ModelState.IsValidField("Email") && ModelState.IsValidField("PhoneNumber"))
+            {
+                if (Context.Customers
+                    .Any(c => c.Name == customer.Name &&
+                        c.Email == customer.Email &&
+                        c.PhoneNumber == customer.PhoneNumber))
+                {
+                    ModelState.AddModelError("Name", "This combination of Email, Phone Number, and Customer Name already exists!");
+                }
             }
+
+        }
     }
 }
